804b746197d4fad6d1dbe6aadcc6b0bf0b10a8b2
mahdi <mahdi.gareno@gmail.com>
2026-05-18 16:14:40 +0330

    Add Theme

--- melonet-android/app/src/main/java/com/example/melonet/presentation/theme/Color.kt
+++ melonet-android/app/src/main/java/com/example/melonet/presentation/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.melonet.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryDark = Color(0xFFFFD700)
+val BackgroundDark = Color(0xFF000000)
+val SurfaceDark = Color(0xFF1A1A1A)
+val OnPrimaryDark = Color(0xFF000000)
+val OnBackgroundDark = Color(0xFFF5F5F5)
+val OnSurfaceDark = Color(0xFFF5F5F5)
+
+
+val PrimaryLight = Color(0xFFF59E0B)
+val BackgroundLight = Color(0xFFF1F5F9)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val OnBackgroundLight = Color(0xFF1E293B)
+val OnSurfaceLight = Color(0xFF1E293B)
