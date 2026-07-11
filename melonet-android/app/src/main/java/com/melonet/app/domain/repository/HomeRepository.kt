d3b4720735526b2d308165e890d396f8decedb37
mahdi <mahdi.gareno@gmail.com>
2026-07-12 02:00:07 +0330

    refactor(song_repo): streamline song retrieval logic by consolidating query functions and enhancing error handling

--- melonet-android/app/src/main/java/com/melonet/app/domain/repository/HomeRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.melonet.app.domain.repository
-
-import com.melonet.app.core.common.Result
-import com.melonet.app.domain.model.HomeFeed
-
-interface HomeRepository {
-    suspend fun getHomeFeed(): Result<HomeFeed>
-}
